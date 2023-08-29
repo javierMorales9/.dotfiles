68dab64a4566722af137edf767bae432cde8058b
Javier Morales de Vera <javierMorales9@gmail.com>
2023-08-29 09:51:17 +0200

    fix a telescope issue

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -5,25 +5,23 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find Files"})
+  builtin.grep_string({ search = vim.fn.input("Grep >") });
+end)
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find Files" })
 vim.keymap.set("n", "<leader>re",
   [[<cmd>lua require('telescope').extensions.recent_files.pick()<CR>]],
-  {noremap = true, silent = true})
-
-	builtin.grep_string({ search = 	vim.fn.input("Grep >") });
-end)
+  { noremap = true, silent = true })
 
 
-telescope.setup{
-    defaults = {
-        mappings = {
-            i = {
-                ["<A-j>"] = actions.move_selection_next,
-                ["<A-k>"] = actions.move_selection_previous,
-            }
-        }
-    },
-    pickers = {},
-    extensions = {},
+telescope.setup {
+  defaults = {
+    mappings = {
+      i = {
+        ["<A-j>"] = actions.move_selection_next,
+        ["<A-k>"] = actions.move_selection_previous,
+      }
+    }
+  },
+  pickers = {},
+  extensions = {},
 }
-
